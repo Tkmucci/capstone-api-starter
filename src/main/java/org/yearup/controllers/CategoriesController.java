09f2b77d9dc72eda12e05f9ee817687f1f406142
tkmucci <takudzwamucherengi@gmail.com>
2026-06-19 14:55:48 -0500

    25th commit: fixed the addCategory method in CategoriesController to return a ResponseEntity with status 201 Created.

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -71,8 +72,8 @@ public class CategoriesController
     public ResponseEntity<Category> addCategory(@RequestBody Category category)
     {
         // insert the category and return it with status 201 Created
-        categoryService.create(category);
-        return ResponseEntity.ok(category);
+        Category created = categoryService.create(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
